33580d99560e3551a9e7a6edf6ec5097cca579ba
tim21821 <tim.mattick@gmx.de>
2023-04-15 10:42:58 +0200

    Add function to play move and to play sequence

--- src/main.rs
+++ src/main.rs
@@ -18,4 +18,19 @@ impl Position {
         };
     }
 
+    fn from_sequence(seq: &str) -> Self {
+        let mut p = Self::new();
+        for c in seq.chars() {
+            let col: usize = (c.to_digit(10).unwrap() - 1) as usize;
+            p.play(col);
+        }
+        return p;
+    }
+
+    fn play(&mut self, col: usize) {
+        self.board[self.height[col]][col] = self.current;
+        self.height[col] += 1;
+        self.num_moves += 1;
+        self.current *= -1;
+    }
 }
